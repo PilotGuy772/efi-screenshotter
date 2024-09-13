fab3fc57b81595d7f235aaf36bb8104fc27cc7ea
PilotGuy772 <laethenglish@gmail.com>
2024-09-12 19:43:57 -0700

    reads key input

--- main.c
+++ main.c
@@ -10,8 +10,16 @@ efi_main(EFI_HANDLE imageHandle, EFI_SYSTEM_TABLE *systemTable)
     Print(L"Hello, World!");
 
     // Wait for a key press
-    EFI_INPUT_KEY Key;
+    EFI_INPUT_KEY key;
     uefi_call_wrapper(systemTable->ConIn->Reset, 2, systemTable->ConIn, FALSE);
-    while (uefi_call_wrapper(systemTable->ConIn->ReadKeyStroke, 2, systemTable->ConIn, &Key) == EFI_NOT_READY);
+
+    
+    while (1) {
+        EFI_STATUS status = uefi_call_wrapper(systemTable->ConIn->ReadKeyStroke, 2, systemTable->ConIn, &key);
+        if (status == EFI_SUCCESS) {
+            Print(L"Key pressed: ScanCode=%d, UnicodeChar=%c\n", key.ScanCode, key.UnicodeChar);
+        }
+    }
+
     return EFI_SUCCESS;
 }
