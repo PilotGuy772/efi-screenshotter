cad58aa4868b1466c0b3294859e20d6c55fe4d3e
PilotGuy772 <laethenglish@gmail.com>
2024-09-14 21:43:33 -0700

    rewrite; working file system!

--- main.c
+++ main.c
@@ -1,135 +1,144 @@
 #include <efi.h>
 #include <efilib.h>
-#include <string.h>
 
-EFI_STATUS status;
-EFI_FILE_PROTOCOL *Root;
-EFI_FILE_PROTOCOL *File;
-EFI_SIMPLE_FILE_SYSTEM_PROTOCOL *SimpleFileSystem;
-const CHAR16 *FileName = L"\\keylog.txt";
-int Terminate = 0;
 
-void s_write_data(CHAR16 *data)
+EFI_DRIVER_BINDING_PROTOCOL gDriverBinding = {
+    Supported,
+    Start,
+    Stop,
+    0x10,
+    NULL,
+    NULL
+};
+
+EFI_STATUS
+EFIAPI
+Supported (
+    IN EFI_DRIVER_BINDING_PROTOCOL *This,
+    IN EFI_HANDLE                  ControllerHandle,
+    IN EFI_DEVICE_PATH_PROTOCOL    *RemainingDevicePath
+    )
 {
-    UINTN size = StrLen(data);
-    status = uefi_call_wrapper(File->Write, 3, File, size, &data);
-    if (status != 0) {
-        Print(L"Unable to write to file\n");
-        Terminate = 1;
-    }
-}
-
-void s_open_file()
-{
-    status = uefi_call_wrapper(BS->LocateProtocol, 3, &gEfiSimpleFileSystemProtocolGuid, NULL, (void **)&SimpleFileSystem);
-    if (EFI_ERROR(status)) {
-        Print(L"Unable to locate Simple File System Protocol\n");
-    }
-
-    // Open the volume
-    status = uefi_call_wrapper(SimpleFileSystem->OpenVolume, 2, SimpleFileSystem, &Root);
-    if (EFI_ERROR(status)) {
-        Print(L"Unable to open volume\n");
-    }
-
-    // Create or open the file
-    status = uefi_call_wrapper(Root->Open, 5, Root, &File, FileName, EFI_FILE_MODE_CREATE | EFI_FILE_MODE_WRITE | EFI_FILE_MODE_READ, 0);
-    if (EFI_ERROR(status)) {
-        Print(L"Unable to open file\n");
-    }
-
-    s_write_data(L"Start Of Log");
-
-}
-
-
-
-void s_process_key(EFI_INPUT_KEY Key) {
-
-    // put in file as either CODE=<code>
-    // or CHAR=<char> followed by newline
-    // if code != 0, put code & no char
-    // otherwise put char
-    
-    CHAR16 *buffer;
-    CHAR16 numStr[5]; // Buffer to hold the string representation of the number
-
-    if (Key.ScanCode != 0) {
-        buffer = (CHAR16 *)AllocatePool(50 * sizeof(CHAR16)); // Allocate more memory
-        if (buffer == NULL) {
-            return; // Handle memory allocation failure
-        }
-
-        // check for ESCAPE
-        if (Key.ScanCode == 23)
-        {
-            StrCpy(buffer, L"End Of Log\n");
-            Terminate = 1;
-        }
-
-        StrCpy(buffer, L"CODE=");
-        UnicodeSPrint(numStr, sizeof(numStr), L"%d", Key.ScanCode); // Convert ScanCode to string
-        StrCat(buffer, numStr);
-        StrCat(buffer, L"\n");
-    } else {
-        buffer = (CHAR16 *)AllocatePool(50 * sizeof(CHAR16)); // Allocate more memory
-        if (buffer == NULL) {
-            return; // Handle memory allocation failure
-        }
-
-        StrCpy(buffer, L"CHAR=");
-
-        if (Key.UnicodeChar == 0x0D) { // newline
-            StrCat(buffer, L"RET");
-        } else if (Key.UnicodeChar == 0x7F) { // delete
-            StrCat(buffer, L"DEL");
-        } else if (Key.UnicodeChar == 0x08) { // backspace
-            StrCat(buffer, L"BSP");
-        } else {
-            UnicodeSPrint(numStr, sizeof(numStr), L"%c", Key.UnicodeChar); // Convert UnicodeChar to string
-            StrCat(buffer, numStr);
-        }
-        StrCat(buffer, L"\n");
-    }
-
-    s_write_data(buffer);
-    FreePool(buffer);
-}
-
-void s_close_file()
-{
-    uefi_call_wrapper(File->Close, 1, File);
+    // Check if the driver supports the device
+    return EFI_SUCCESS;
 }
 
 EFI_STATUS
 EFIAPI
-efi_main(EFI_HANDLE imageHandle, EFI_SYSTEM_TABLE *systemTable)
+Start (
+    IN EFI_DRIVER_BINDING_PROTOCOL *This,
+    IN EFI_HANDLE                  ControllerHandle,
+    IN EFI_DEVICE_PATH_PROTOCOL    *RemainingDevicePath
+    )
 {
-    InitializeLib(imageHandle, systemTable);
+    // Start the driver
+    return EFI_SUCCESS;
+}
 
-    Print(L"Hello, World!\n");
-    Print(L"Opening file...\n");
-    s_open_file();
-    Print(L"Done! Keystrokes will now be saved to esp/keylog.txt...\n");
+EFI_STATUS
+EFIAPI
+Stop (
+    IN EFI_DRIVER_BINDING_PROTOCOL *This,
+    IN EFI_HANDLE                  ControllerHandle,
+    IN UINTN                       NumberOfChildren,
+    IN EFI_HANDLE                  *ChildHandleBuffer
+    )
+{
+    // Stop the driver
+    return EFI_SUCCESS;
+}
 
-    // Wait for a key press
-    EFI_INPUT_KEY key;
-    uefi_call_wrapper(systemTable->ConIn->Reset, 2, systemTable->ConIn, FALSE);
+UINTN AsciiStrLen (IN CONST CHAR8 *String)
+{
+    UINTN Length = 0;
+    while (*String != '\0') {
+        Length++;
+        String++;
+    }
+    return Length;
+}
 
-    
-    while (1) {
-        if(Terminate)
-        {
-            s_close_file();
+EFI_STATUS EFIAPI efi_main(EFI_HANDLE ImageHandle, EFI_SYSTEM_TABLE *SystemTable)
+{
+    EFI_SIMPLE_FILE_SYSTEM_PROTOCOL *FileSystem;
+    EFI_STATUS Status;
+    UINTN HandleCount;
+    EFI_HANDLE *HandleBuffer;
+
+    // Initialize the library
+    InitializeLib(ImageHandle, SystemTable);
+
+    // Locate all handles that support the Simple File System Protocol
+    Status = uefi_call_wrapper(BS->LocateHandleBuffer, 5, ByProtocol, &gEfiSimpleFileSystemProtocolGuid, NULL, &HandleCount, &HandleBuffer);
+    if (EFI_ERROR(Status)) {
+        return Status;
+    }
+
+    // Iterate through the handles to find a valid file system
+    for (UINTN Index = 0; Index < HandleCount; Index++) {
+        Status = uefi_call_wrapper(BS->HandleProtocol, 3, HandleBuffer[Index], &gEfiSimpleFileSystemProtocolGuid, (VOID**)&FileSystem);
+        if (!EFI_ERROR(Status)) {
             break;
-        }
-
-        EFI_STATUS status = uefi_call_wrapper(systemTable->ConIn->ReadKeyStroke, 2, systemTable->ConIn, &key);
-        if (status == EFI_SUCCESS) {
-            Print(L"Key pressed: ScanCode=%d, UnicodeChar=%c\n", key.ScanCode, key.UnicodeChar);
-            s_process_key(key);
         }
     }
 
+    if (EFI_ERROR(Status)) {
+        Print(L"Error initializing the protocol! %d\n", Status);
+        return Status;
+    }
+
+    // open the volume
+    EFI_FILE_PROTOCOL *Root;
+    Status = uefi_call_wrapper(FileSystem->OpenVolume, 2, FileSystem, &Root);
+    if (EFI_ERROR(Status)) {
+        // Handle error
+        Print(L"Error opening the volume!\n");
+        return Status;
+    }
+
+    // open \hello.txt
+    EFI_FILE_PROTOCOL *File;
+    Status = uefi_call_wrapper(Root->Open, 5, Root, &File, L"\\hello.txt", EFI_FILE_MODE_CREATE | EFI_FILE_MODE_READ | EFI_FILE_MODE_WRITE, 0);
+    if (EFI_ERROR(Status)) {
+        // Handle error
+        Print(L"Error opening the file! %d\n", Status);
+        return Status;
+    }
+
+    // write
+    CHAR8 *message = "Hello, World!\n";
+    const UINTN messageSize = AsciiStrLen(message) + 1; // one extra for the null terminator
+    Print(L"Message: %a\n", message);
+    Print(L"Message size: %d bytes.\n", messageSize);
+    Status = uefi_call_wrapper(File->Write, 3, File, &messageSize, message);
+    Print(L"Wrote %d bytes.\n", messageSize);
+    if (EFI_ERROR(Status)) {
+        Print(L"Error writing to the file!\n");
+        return Status;
+    }
+
+    // write again
+    CHAR8* message2 = "Goodbye, World!\n";
+    const UINTN messageSize2 = AsciiStrLen(message2) + 1;
+    Print(L"Message2: %a\n", message2);
+    Print(L"Message size: %d bytes.\n", messageSize2);
+    Print(L"Starting from %d bytes.\n", messageSize);
+    Status = uefi_call_wrapper(File->Write, 3, File, &messageSize2, message2);
+    Print(L"Wrote %d bytes.\n", messageSize2);
+    if (EFI_ERROR(Status)) {
+        Print(L"Error writing to the file!\n");
+    }
+
+    // close file
+    Status = uefi_call_wrapper(File->Flush, 1, File);
+    if (EFI_ERROR(Status)) {
+        Print(L"Error flushing the file!\n");
+    }
+    uefi_call_wrapper(File->Close, 1, File);
+
+
     return EFI_SUCCESS;
 }
+
+
+    
